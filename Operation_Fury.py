e4eab400f0b5c156b5805b16958df53147315b81
Gavin Butcher <gavinbutcher.20@gmail.com>
2022-10-04 10:48:00 -0400

    Imported time library

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -7,7 +7,10 @@ Our Welcome Screen will start our program letting
 drivers know that the InfoTechCenter OS is loading
 """
 
+#Import Libraries Here
+from time import sleep #imported sleep function from the time library 
+
 
 print("\nWelcome to Operation Fury InfoTech Center")
-
+sleep(2)
 print("\nOperation Fury Operation system booting up")
