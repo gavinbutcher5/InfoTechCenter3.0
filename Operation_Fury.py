dcf5cace36dcc9176fa0214cf4bb111c8d0adf06
Gavin Butcher <gavinbutcher.20@gmail.com>
2022-10-04 10:39:36 -0400

    Add print statements

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -6,3 +6,8 @@
 Our Welcome Screen will start our program letting
 drivers know that the InfoTechCenter OS is loading
 """
+
+
+print("\nWelcome to Operation Fury InfoTech Center")
+
+print("\nOperation Fury Operation system booting up")
