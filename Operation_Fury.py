b8bfc721e258815351cc1bd6215d929c085c8de3
Gavin Butcher <gavinbutcher.20@gmail.com>
2022-10-04 10:50:06 -0400

    Changed text color to bright green

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -8,9 +8,10 @@ drivers know that the InfoTechCenter OS is loading
 """
 
 #Import Libraries Here
-from time import sleep #imported sleep function from the time library 
+from time import sleep #imported sleep function from the time library
 
 
 print("\nWelcome to Operation Fury InfoTech Center")
 sleep(2)
 print("\nOperation Fury Operation system booting up")
+print("\033[1;32m This text is Bright Green  \n")
