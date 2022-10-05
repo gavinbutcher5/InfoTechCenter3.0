1ab5c63cf225df2bc91b7cd2602f592e6d3d5ec3
Gavin Butcher <gavinbutcher.20@gmail.com>
2022-10-05 08:38:53 -0400

    Intial Weather commit

---
+++ Operation_Fury.py
@@ -0,0 +1,8 @@
+#weather
+#Developer:Gavin Butcher
+#version 1.0
+
+"""
+Create a function for our current weather using the random. choice function to determine what the weather is
+picking from a list - using If, Elif and else statements to check the condition and print a specific print line
+"""
