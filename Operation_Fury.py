22f604658b1afd72b62e28c456cdeeb413b4479c
Gavin Butcher <gavinbutcher.20@gmail.com>
2022-10-05 08:22:05 -0400

    Final Os booting up

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -10,11 +10,11 @@ drivers know that the InfoTechCenter OS is loading
 #Import Libraries Here
 from time import sleep #imported sleep function from the time library
 
+print("\033[1;31m welcome fury Infotech Center \n")
 
-print("\nWelcome to Operation Fury InfoTech Center")
 sleep(2)
 print("\033[1;32m Operation Fury Operation system booting up  \n")
 
 for i in range(2):
-    print("OS Booting up")
+    print("OS Booting up.")
     sleep(1)
