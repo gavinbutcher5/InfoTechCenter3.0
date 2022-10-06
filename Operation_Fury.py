73a8a0f5c73ce63a30c51abc4561830803f56d4e
Gavin Butcher <gavinbutcher.20@gmail.com>
2022-10-06 08:38:26 -0400

    Define the Weather Function

--- Operation_Fury.py
+++ Operation_Fury.py
@@ -6,3 +6,16 @@
 Create a function for our current weather using the random. choice function to determine what the weather is
 picking from a list - using If, Elif and else statements to check the condition and print a specific print line
 """
+
+#Import Libraries here
+import random
+
+#Weather condition list using the random.choice library
+#to randomly choose a condition and storing it in its brain
+def weather():
+    weather_forcast = [" Rain","Snow", "Sunny", "Cloudy", "Foggy", "Storming", "Icy"]
+    weather_condition = random.choice(weather_forcast)
+    return weather_condition
+
+
+
